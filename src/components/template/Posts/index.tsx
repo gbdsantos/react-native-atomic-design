0617b699569f4137dcea99d1e48aacf8294b9626
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-08-02 16:06:54 -0300

    :recycle: refactor(mobile): apply atomic design transform models/environment to organisms

--- src/components/template/Posts/index.tsx
+++ src/components/template/Posts/index.tsx
@@ -7,16 +7,7 @@ import {
   View
 } from 'react-native';
 
-import {
-  MessengerIcon,
-  FavoriteIcon,
-  CommentIcon,
-  CameraIcon,
-  LogoIcon,
-  IgtvIcon,
-  SaveIcon,
-  ProfileIcon
-} from '../../../global/styles/icons';
+import { Post } from '../../organisms/Post';
 
 import { posts } from '../../../utils/posts';
 
@@ -30,35 +21,7 @@ export function Posts(){
     showsVerticalScrollIndicator={false}
     keyExtractor={post => post.id}
     renderItem={({ item }) => (
-      <View style={styles.post}>
-        <View style={styles.postHeader}>
-          <Image source={ProfileIcon} style={styles.postAvatar} />
-          <View>
-            <Text style={styles.postUsername}>Rodrigo Gonçalves</Text>
-            <Text style={styles.postLocation}>Somewhere</Text>
-          </View>
-        </View>
-
-        <Image source={item.cover} style={styles.cover} />
-
-        <View style={styles.postFooter}>
-          <View style={styles.postOptions}>
-            <View style={styles.postOptionsSide}>
-              <FavoriteIcon style={styles.postOptionsIcon} />
-              <CommentIcon style={styles.postOptionsIcon} />
-              <MessengerIcon style={styles.postOptionsIcon} />
-            </View>
-
-            <SaveIcon />
-          </View>
-
-          <View style={styles.postAbout}>
-            <Image source={item.lastLiked.avatar} style={styles.lastLiked} />
-            <Text style={styles.likes}>{item.likes}</Text>
-          </View>
-          <Text style={styles.description}>{item.description}</Text>
-        </View>
-      </View>
+      <Post data={item} />
     )}
   />
   );
